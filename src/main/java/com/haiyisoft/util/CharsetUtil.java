bafa19bb913fc94edf59d69366d8e11a6e79ccdf
chryl <1015670600@qq.com>
2023-10-25 09:00:20 +0800

    charset

---
+++ src/main/java/com/haiyisoft/util/CharsetUtil.java
@@ -0,0 +1,19 @@
+package com.haiyisoft.util;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created By Chryl on 2023-10-25.
+ */
+public class CharsetUtil {
+
+    /**
+     * 获取GBK编码字符集
+     *
+     * @return
+     */
+    public static Charset charsetGBK() {
+        return Charset.forName("gbk");
+    }
+
+}
