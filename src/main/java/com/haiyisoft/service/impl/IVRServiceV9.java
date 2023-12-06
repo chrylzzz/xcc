6e821952e26ca1cd7946a921d1ed2e85956a3daf
chryl <cyl5yy412@gmail.com>
2023-12-06 10:00:55 +0800

    后缀码优化

--- src/main/java/com/haiyisoft/service/impl/IVRServiceV9.java
+++ src/main/java/com/haiyisoft/service/impl/IVRServiceV9.java
@@ -132,7 +132,7 @@ public class IVRServiceV9 implements IVRService {
             log.info("hangup this call channelId: {} ,icdCallerId: {}", channelId, icdCallerId);
 
             log.info("this call completed: {} , {}", ivrEvent, ngdEvent);
-            IVRHandler.afterHangupNotTransfer(ivrEvent, ngdEvent);
+//            IVRHandler.afterHangupNotTransfer(ivrEvent, ngdEvent);
 
         }
     }
