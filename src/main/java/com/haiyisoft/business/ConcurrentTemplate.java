f120918cbbc0e282acf68657ba882999eab8649f
chryl <cyl5yy412@gmail.com>
2024-01-08 14:21:31 +0800

    asr并发

--- src/main/java/com/haiyisoft/business/ConcurrentTemplate.java
+++ src/main/java/com/haiyisoft/business/ConcurrentTemplate.java
@@ -1,10 +1,6 @@
 package com.haiyisoft.business;
 
 import com.haiyisoft.chryl.ivr.DispatcherIVR;
-import com.haiyisoft.entry.ChannelEvent;
-import com.haiyisoft.entry.IVREvent;
-import com.haiyisoft.entry.NGDEvent;
-import io.nats.client.Connection;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
